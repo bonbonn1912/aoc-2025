e172ae9fae36331422dc5852aa4b52792d216820
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2025-12-12 19:24:05 +0100

    Initial commit

---
+++ src/day01/solution.rs
@@ -0,0 +1,10 @@
+use std::fs;
+pub fn print_solution() {
+    let file_path = "input/day01.txt";
+    println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use crate::day01::solution::print_solution;
+mod day01 {
+    pub mod solution;
+}
+
+fn main() {
+    print_solution()
+}
